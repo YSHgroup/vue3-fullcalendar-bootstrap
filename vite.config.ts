d74cf794ceef2a53dbb514f57aa2350ec623e26e
YSHgroup <david.dev100@outlook.com>
2024-02-08 12:20:51 -0500

    Install packages fullcalendar and scss

--- vite.config.ts
+++ vite.config.ts
@@ -19,5 +19,12 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@scss/global.scss"`
+      }
+    }
   }
 })
