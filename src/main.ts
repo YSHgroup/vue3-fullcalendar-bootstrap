114234812cb55880b4f4673fa74d464ccef58f12
YSHgroup <david.dev100@outlook.com>
2024-02-08 11:36:37 -0500

    Configure to use the bootstrap-vue-next

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
 import './assets/main.css'
 
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
+
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createBootstrap} from 'bootstrap-vue-next'
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +15,7 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+app.use(createBootstrap())
 
 app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,17 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import Components from 'unplugin-vue-components/vite'
+import {BootstrapVueNextResolver} from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+    Components({
+      resolvers: [BootstrapVueNextResolver()] // Automatically register the bootstrap components globally
+    })
   ],
   resolve: {
     alias: {
