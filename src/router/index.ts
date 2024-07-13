28c035a0294f77d9825ecc27aef0cf50194aaa4d
Magic Sky <146746648+YSHgroup@users.noreply.github.com>
2024-07-13 04:50:38 -0400

    Add the afterEach method to router to auto-change title

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { nextTick } from 'vue'
 import routes from './routes'
 
+const BASE_TITLE = "Calendar - Vue3"
 const router = createRouter({
   scrollBehavior: () => ({top: 0, left: 0}),
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,6 +14,16 @@ router.beforeEach((to, from, next) => {
     document.title = to.meta.title
   }
   next()
+})
+
+router.afterEach(( to, from )=>{
+  if(to.meta.title) {
+    nextTick(() => {
+      document.title = `${to.meta.title} | ${BASE_TITLE}`
+    })
+  } else {
+    document.title = BASE_TITLE
+  }
 })
 
 export default router
